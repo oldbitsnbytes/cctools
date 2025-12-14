b8bd48d346bd3c684509323196806a641efada5c
oldbitsnbytes <lussier.serge@gmail.com>
2025-12-14 10:53:33 -0500

    Update main.cxx to use cctools logging and journaling features

--- c/main.cxx
+++ c/main.cxx
@@ -3,12 +3,14 @@
 //
 
 #include <cctools/mstr.h>
+#include <cctools/journal.h>
+
 
 
 auto main(int argc, char** argv) -> int
 {
-    cpp::mstr str;
-    str << "Hello World!";
-    std::cout << str() << std::endl;
+    sys::message() << rem::fn::func << color::aqua << "cctools" << color::white << "::" << color::deeppink8 << "main" << color::yellow  << "()" << sys::eol;
+    sys::log()  << "Hello World!" << sys::eol;
+    sys::flush("main.journal");
     return 0;
 }
